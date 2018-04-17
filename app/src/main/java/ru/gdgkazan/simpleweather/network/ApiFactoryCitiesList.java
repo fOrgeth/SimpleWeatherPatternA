a170178710285377fd1e5dc2ea7070ac89e33118
fOrgeth <ivdovin@mail.ru>
2018-04-17 22:02:08 +0300

    ApiFactory added, ListService added

---
+++ app/src/main/java/ru/gdgkazan/simpleweather/network/ApiFactoryCitiesList.java
@@ -0,0 +1,67 @@
+package ru.gdgkazan.simpleweather.network;
+
+import android.support.annotation.NonNull;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import ru.gdgkazan.simpleweather.BuildConfig;
+
+/**
+ * @author Artur Vasilov
+ */
+public final class ApiFactoryCitiesList {
+
+    private static OkHttpClient sClient;
+
+    private static volatile CitiesListService sService;
+
+    private ApiFactoryCitiesList() {
+    }
+
+    @NonNull
+    public static CitiesListService getWeatherService() {
+        CitiesListService service = sService;
+        if (service == null) {
+            synchronized (ApiFactoryCitiesList.class) {
+                service = sService;
+                if (service == null) {
+                    service = sService = buildRetrofit().create(CitiesListService.class);
+                }
+            }
+        }
+        return service;
+    }
+
+    @NonNull
+    private static Retrofit buildRetrofit() {
+        return new Retrofit.Builder()
+                .baseUrl(BuildConfig.API_HELP_LIST)
+                .client(getClient())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    @NonNull
+    private static OkHttpClient getClient() {
+        OkHttpClient client = sClient;
+        if (client == null) {
+            synchronized (ApiFactoryCitiesList.class) {
+                client = sClient;
+                if (client == null) {
+                    client = sClient = buildClient();
+                }
+            }
+        }
+        return client;
+    }
+
+    @NonNull
+    private static OkHttpClient buildClient() {
+        return new OkHttpClient.Builder()
+                .addInterceptor(new HttpLoggingInterceptor())
+                .addInterceptor(new ApiKeyInterceptor())
+                .build();
+    }
+}
--- app/src/main/java/ru/gdgkazan/simpleweather/network/WeatherService.java
+++ app/src/main/java/ru/gdgkazan/simpleweather/network/WeatherService.java
@@ -2,17 +2,21 @@ package ru.gdgkazan.simpleweather.network;
 
 import android.support.annotation.NonNull;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 import ru.gdgkazan.simpleweather.data.model.City;
 
-/**
- * @author Artur Vasilov
- */
+
 public interface WeatherService {
 
     @GET("data/2.5/weather?units=metric")
     Call<City> getWeather(@NonNull @Query("q") String query);
+
+    @GET("data/2.5/group?units=metric")
+    Call<List<City>> getWeatherList(@NonNull @Query("id") String query);
+
 
 }
